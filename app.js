e340488fc10a770be6c4f5c597a23c364067f6e1
John Dinh <johnduydinh@gmail.com>
2019-09-19 17:38:27 -0400

    Add dotenv package & IPStack API

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
+require('dotenv').config();
 const request = require('request');
 const moment = require('moment-timezone');
 const convertTime12to24 = require('./functions/convertTime12to24');
 const utcTimezoneConverter = require('./functions/utcTimezoneConverter');
 
+const ipStackKey = process.env.IPSTACK_API_KEY;
+const ipStackAPI = `http://api.ipstack.com/104.218.40.246?access_key=${ipStackKey}&format=1`;
+request(ipStackAPI, (error, response, body) => {
+    const latitdue = JSON.parse(body).latitude;
+    const longitude = JSON.parse(body).longitude;
+    const apiLink = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today`;
+    console.log('Today\'s sunrise was...');
+    request(apiLink, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const data = JSON.parse(body).results;
+            const sunrise24hr = convertTime12to24(data.sunrise);
+            const sunset24hr = convertTime12to24(data.sunset);
+            const timezone = 'America/New_York' // Maybe automatically get this data from user for furture improvement?
+
+            const sunriseTime = utcTimezoneConverter(sunrise24hr, timezone);
+            const sunsetTime = utcTimezoneConverter(sunset24hr, timezone);
+
+            console.log(sunriseTime);
+            console.log(`Sunset today will be ${sunsetTime}`);
+
+
+        } else {
+            console.log('Something went wrong, please check your internet connection');
+        }
+
+    });
+
+});
+
 // Maybe get this data automatically from users for future improvement
 const latitude = 39.91341905;
 const longitude = -75.5752288905074;
-const apiLink = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today`;
 
 moment.tz.setDefault('Universal');
 
-console.log('Today\'s sunrise was...');
-request(apiLink, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        const data = JSON.parse(body).results;
-        const sunrise24hr = convertTime12to24(data.sunrise);
-        const sunset24hr = convertTime12to24(data.sunset);
-        const timezone = 'America/New_York' // Maybe automatically get this data from user for furture improvement?
-
-        const sunriseTime = utcTimezoneConverter(sunrise24hr, timezone);
-        const sunsetTime = utcTimezoneConverter(sunset24hr, timezone);
-
-        console.log(sunriseTime);
-        console.log(`Sunset today will be ${sunsetTime}`);
 
 
-    } else {
-        console.log('Something went wrong, please check your internet connection');
-    }
-
-});
 
 // Code I originally wrote inside request before refactoring
     // const sunriseUTC = convertTime12to24(data.sunrise);
